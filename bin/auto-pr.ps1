5411a011b127fa7e9ba5c685427cf7cab5bbe0a3
沙漠之子 <7850715+maboloshi@users.noreply.github.com>
2023-10-21 13:17:08 +0800

    重新设置项目路径

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "maboloshi/Scoop-Bucket:master"
+    [String]$upstream = "maboloshi/scoop-private:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
